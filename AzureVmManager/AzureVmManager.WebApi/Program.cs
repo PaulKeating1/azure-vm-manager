4b65a18602d513845fd17385d77ec7d8bc70e7f2
Paul Keating <paul.keating@journeyintocode.com>
2024-07-25 16:49:45 +0100

    Add Scrutor
    Added Scrutor to register services in WebAPI.

--- AzureVmManager/AzureVmManager.WebApi/Program.cs
+++ AzureVmManager/AzureVmManager.WebApi/Program.cs
@@ -1,8 +1,16 @@
+using AzureVmManager.Services.Implementations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.Scan(s =>
+{
+    s.FromAssemblyOf<ArmClientFactory>().AddClasses().AsMatchingInterface().WithScopedLifetime();
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
