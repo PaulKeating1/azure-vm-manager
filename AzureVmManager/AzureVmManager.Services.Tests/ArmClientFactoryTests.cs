7cc440ab1b9463aa16b75ebadf06b557b7200b7b
Paul Keating <paul.keating@journeyintocode.com>
2024-07-25 16:42:05 +0100

    Add factory to create ArmClient
    Factory and tests created for factory to create an ArmClient. Using a
    factory pattern allows for a mocked ArmClient to be used in unit tests.

---
+++ AzureVmManager/AzureVmManager.Services.Tests/ArmClientFactoryTests.cs
@@ -0,0 +1,21 @@
+﻿using AzureVmManager.Services.Implementations;
+using FluentAssertions;
+
+namespace AzureVmManager.Services.Tests
+{
+    internal class ArmClientFactoryTests
+    {
+        [Test]
+        public void Should_CreateClient()
+        {
+            // Arrange
+            var armClientFactory = new ArmClientFactory();
+
+            // Act
+            var client = armClientFactory.CreateClient();
+
+            // Assert
+            client.Should().NotBeNull();
+        }
+    }
+}
---
+++ AzureVmManager/AzureVmManager.Services/Implementations/ArmClientFactory.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Identity;
+using Azure.ResourceManager;
+using Elector8EnvironmentManager.Services.Interfaces;
+
+namespace AzureVmManager.Services.Implementations
+{
+    public class ArmClientFactory : IArmClientFactory
+    {
+        public ArmClient CreateClient()
+        {
+            var client = new ArmClient(new DefaultAzureCredential());
+            return client;
+        }
+    }
+}
