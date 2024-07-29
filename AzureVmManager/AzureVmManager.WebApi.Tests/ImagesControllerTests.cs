a3b41ced30f102b65b0139414700fac0355c9a5f
Paul Keating <paul.keating@journeyintocode.com>
2024-07-29 08:57:35 +0100

    Add WebAPI endpoint to get VM images
    WebAPI endpoint and tests created to return all VM images for the
    specified subscription. The endpoint will be called by the frontend and
    displayed to users, allowing them to create VMs from images or delete
    images.

---
+++ AzureVmManager/AzureVmManager.WebApi.Tests/ImagesControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using AzureVmManager.DataObjects;
+using AzureVmManager.Services.Interfaces;
+using AzureVmManager.WebApi.Controllers;
+using FluentAssertions;
+using Moq;
+
+namespace AzureVmManager.WebApi.Tests
+{
+    internal class ImagesControllerTests
+    {
+        [Test]
+        [TestCase(0)]
+        [TestCase(2)]
+        public void Should_GetImages(int numberOfImages)
+        {
+            // Arrange
+            var subscriptionId = Guid.NewGuid().ToString();
+            const string resourceGroupName = "test-resource-group";
+            const string galleryName = "test-gallery";
+            var imageCollection = Enumerable.Range(0, numberOfImages).Select(x => new Image { Location = "uksouth", Name = "", Offer = "", Publisher = "", Sku = "" });
+            var getImagesServiceMock = new Mock<IGetImagesService>();
+            getImagesServiceMock.Setup(x => x.GetImages(subscriptionId, resourceGroupName, galleryName)).Returns(imageCollection);
+            var imagesController = new ImagesController(getImagesServiceMock.Object);
+
+            // Act
+            var images = imagesController.Get(subscriptionId, resourceGroupName, galleryName);
+
+            // Assert
+            images.Should().NotBeNull();
+            images.Count().Should().Be(numberOfImages);
+        }
+    }
+}
---
+++ AzureVmManager/AzureVmManager.WebApi/Controllers/ImagesController.cs
@@ -0,0 +1,24 @@
+﻿using AzureVmManager.DataObjects;
+using AzureVmManager.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureVmManager.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImagesController : ControllerBase
+    {
+        private IGetImagesService _getImagesService;
+
+        public ImagesController(IGetImagesService getImagesService)
+        {
+            _getImagesService = getImagesService;
+        }
+
+        public IEnumerable<Image> Get(string subscriptionId, string resourceGroupName, string galleryName)
+        {
+            var images = _getImagesService.GetImages(subscriptionId, resourceGroupName, galleryName);
+            return images;
+        }
+    }
+}
